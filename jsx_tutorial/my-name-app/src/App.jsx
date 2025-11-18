4085c16f6a382814f805a5f58f8722f3da5468c6
Sourav Singh <55342631+100ravSingh@users.noreply.github.com>
2025-11-18 18:18:22 +0000

    feat: myfirst app

--- jsx_tutorial/my-name-app/src/App.jsx
+++ jsx_tutorial/my-name-app/src/App.jsx
@@ -1,8 +1,11 @@
 function App() {
+  const today = new Date().toDateString();
   return (
     <div>
       <h1>Sourav Singh</h1>
       <p>Welcome to my React App!</p>
+      <p>Today's date is: {today}</p>
+      <p> let's do some math : 5 + 10 = {5 + 10}</p>
     </div>
   );
 }
