157af4bc62c136a3691e5bb074701fbcb27e8a11
Sourav Singh <55342631+100ravSingh@users.noreply.github.com>
2025-11-18 18:22:14 +0000

    feat: myfirst app

--- jsx_tutorial/my-name-app/src/App.jsx
+++ jsx_tutorial/my-name-app/src/App.jsx
@@ -1,3 +1,5 @@
+import Greet from "./greet";
+
 function App() {
   const today = new Date().toDateString();
   return (
@@ -6,6 +8,7 @@ function App() {
       <p>Welcome to my React App!</p>
       <p>Today's date is: {today}</p>
       <p> let's do some math : 5 + 10 = {5 + 10}</p>
+      <Greet />
     </div>
   );
 }
---
+++ jsx_tutorial/my-name-app/src/greet.jsx
@@ -0,0 +1,5 @@
+function greet() {
+  return <h1>Have a great day learning React!</h1>;
+}
+
+export default greet;
